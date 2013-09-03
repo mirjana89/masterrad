0848a37de82f587e00166d2346a2ff64bb3afadd
mirjana89 <mirjana.matovic89@hotmail.com>
2013-09-03 15:13:41 +0200

    Create project benchmark-tool

---
+++ benchmark-tool/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to benchmark-tool
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ benchmark-tool/test/benchmark_tool/core_test.clj
@@ -0,0 +1,7 @@
+(ns benchmark-tool.core-test
+  (:require [clojure.test :refer :all]
+            [benchmark-tool.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
