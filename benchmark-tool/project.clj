c346d135c0024cc791bf9d3a7bd53b3144a592c8
mirjana89 <mirjana.matovic89@hotmail.com>
2013-09-03 15:21:21 +0200

    Initial configuration of project.clj, eclipse support

--- benchmark-tool/project.clj
+++ benchmark-tool/project.clj
@@ -1,6 +1,7 @@
 (defproject benchmark-tool "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Benchmark tool for evaluation of Datomic database"
+  :url "https://github.com/mirjana89/masterrad/tree/master/benchmark-tool"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein2-eclipse "2.0.0"]])
